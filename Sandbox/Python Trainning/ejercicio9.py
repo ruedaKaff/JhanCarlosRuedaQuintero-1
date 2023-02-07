b9bea748a79e5821f50cd0e810b2162243ceb051
ruedaKaf <jhanru@live.com>
2023-02-07 09:27:56 -0500

    ejercicio9 incompleto

---
+++ Sandbox/Python Trainning/ejercicio9.py
@@ -0,0 +1,14 @@
+""" 9. Construya un algoritmo en Python, que permita ingresar la
+información de un empleado e imprima el nombre, los
+apellidos y la antigüedad. Los datos que se deben solicitar
+son los siguientes:
+*Nombre * Teléfono *Año de ingreso a la empresa
+*Apellidos *Edad. """
+
+nombre= input('Cual es tu nombre ?')
+nombre= input('Cual es tu nombre ?')
+nombre= input('Cual es tu nombre ?')
+
+nombre= input('Cual es tu nombre ?')
+
+nombre= input('Cual es tu nombre ?')
