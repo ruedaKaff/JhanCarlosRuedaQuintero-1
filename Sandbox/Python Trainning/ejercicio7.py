9b29b5b1785070d004045e04f48590494dc74539
ruedaKaf <jhanru@live.com>
2023-02-07 09:17:33 -0500

    ejercicio7

---
+++ Sandbox/Python Trainning/ejercicio7.py
@@ -0,0 +1,4 @@
+""" 7. Cuál es la diferencia entre un condicional simple y un
+condicional compuesto? """
+
+print(f"La prinicipal diferencia es que el condicional simple ejecuta 1 posible proceso el complejo 2 o más Esto quiere decir que posee una instruccion en el si y una en el si no")
