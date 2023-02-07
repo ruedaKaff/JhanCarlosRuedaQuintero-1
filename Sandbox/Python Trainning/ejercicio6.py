dd8576a8f64a315b3725717115d470b84196d0c9
ruedaKaf <jhanru@live.com>
2023-02-07 09:11:33 -0500

    Ejercicio6 python

---
+++ Sandbox/Python Trainning/ejercicio6.py
@@ -0,0 +1,9 @@
+""" 6. En sus propias palabras, qué son las funciones
+preconstruidas y proporcione 2 ejemplos. """
+
+
+
+datos=[35, 22, 1, 1025,2]
+valorM = max(datos)
+
+print(f"Las funciones predefinidas como su nombre lo indica estan ya previamente escritas dentro del lenguaje de pyhton y \n Se pueden llamar y utilizar de manera nativa que cubren funciones basicas para crear procesos mas complejos \n Ejemplo -->Encuentre el numero mayor de 'datos:' {datos} = 'max(datos)'= {valorM}")
