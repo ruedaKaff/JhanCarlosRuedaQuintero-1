cfe19371dc892e6ad6c8ef92bbc10c3d0ead73e2
ruedaKaf <jhanru@live.com>
2023-02-07 08:06:53 -0500

    Ejercicio3 python

--- Sandbox/Python Trainning/ejercicio2.py
+++ Sandbox/Python Trainning/ejercicio2.py
@@ -1,4 +1,4 @@
 """ 2. En la jerarquía de operadores, cuáles son los que más
 prioridad tienen cuando el intérprete de Python los evalúa? """
 
-print(f"Jerarquia de operadores Mayor-->Menor \n 1.Operaciones entre parentesis: '()'\n 2.Potencia: '**' \n 3.Multiplicación y División, módulo o residuo, División entera : '*, /, %, //'" )
+print(f"\tJerarquia de operadores Mayor-->Menor \n 1.Operaciones entre parentesis: '()'\n 2.Potencia: '**' \n 3.Multiplicación y División, módulo o residuo, División entera : '*, /, %, //'" )
---
+++ Sandbox/Python Trainning/ejercicio3.py
@@ -0,0 +1,12 @@
+""" 3. Si hay operadores de igual precedencia, en qué orden se
+ejecutan?
+
+A. De izquierda a derecha
+B. De derecha a izquierda """
+
+numero1 = 10
+numero2 = 2
+numero3 = 3
+operacion= numero1 - numero2 + numero3
+
+print (f"B. De izquierda a derecha \n Ejemplo --> la suma y la resta {numero1}-{numero2} + {numero3} = {operacion}")
