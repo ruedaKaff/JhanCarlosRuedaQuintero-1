b33cc2183f45e84731acbca55631ef5f0c22a3a5
ruedaKaf <jhanru@live.com>
2023-02-10 13:25:13 -0500

    ejercicio9

--- Sandbox/Python Trainning/ejercicio9.py
+++ Sandbox/Python Trainning/ejercicio9.py
@@ -4,11 +4,20 @@ apellidos y la antigüedad. Los datos que se deben solicitar
 son los siguientes:
 *Nombre * Teléfono *Año de ingreso a la empresa
 *Apellidos *Edad. """
+import datetime
+x = datetime.datetime.now()
+xyears =x.year
 
-nombre= input('Cual es tu nombre ?')
-nombre= input('Cual es tu nombre ?')
-nombre= input('Cual es tu nombre ?')
+nombre= input('Cual es tu nombre ? :')
 
-nombre= input('Cual es tu nombre ?')
+telefono= input('ingresa tu telefono :')
 
-nombre= input('Cual es tu nombre ?')
+añoIngreso= int(input('Año de ingreso a la empresa :'))
+
+apellidos= input('Cual es tu apellido :')
+
+edad= input('Cual es tu edad? :')
+
+añosAntiguedad = (xyears - añoIngreso )
+print (f"Tu nombre es: {nombre} {apellidos} y tiene {añosAntiguedad} ")
+
