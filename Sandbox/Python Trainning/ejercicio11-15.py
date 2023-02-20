a15534c1d37035afcd80b4e33f1620380f6bca22
ruedaKaf <jhanru@live.com>
2023-02-19 20:57:12 -0500

    ejercicio12.py

--- Sandbox/Python Trainning/ejercicio11-15.py
+++ Sandbox/Python Trainning/ejercicio11-15.py
@@ -1,3 +1,4 @@
+
 """ 1. Campus requiere administrar algunos datos de sus Campers
 como por ejemplo, la creación, eliminación o búsqueda de los
 developers, entre otros, por tal razón, ha solicitado el diseño de
@@ -67,12 +68,37 @@ while opc !=3:
 
     else:
         print ('------------------------------\n Ingrese una opcion valida' )
-
         
+""" 
+2. N atletas han pasado a finales en salto triple en los juegos
+olímpicos de 2022.
+Diseñe un programa que pida por teclado los nombres de cada
+atleta finalista y a su vez, sus marcas del salto en metros.
+Informar el nombre de la atleta campeona que se quede
+con la medalla de oro y si rompió récord, reportar el pago que
+será de 500 millones. El récord esta en 15,50 metros.
 
+"""
 
+atletas = {
 
+}
+
+record = 15.50
+premio = 500
+
+a = int(input("Ingrese la cantidad de participantes: "))
+
+for i in range(a):
+    nombreAtleta = input("Nombre del atleta : ")
+    salto = float(input("Cuantos metros salto : "))
+    atletas[nombreAtleta] = salto
     
-
-
-
+mayor =  max(atletas, key=atletas.get)
+print('DEBUGGIN/////////')
+print(atletas)
+print(mayor)
+if atletas[mayor] > record:
+    print(f"El atleta ganador es: {mayor} con un salto de {atletas[mayor]} metros {premio} millones")
+else:
+    print(f"El atleta ganador es: {mayor} no supero el record")
