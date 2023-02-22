62624adaa1f23e1e3edec6f6d8cd01045457ad65
ruedaKaf <jhanru@live.com>
2023-02-22 07:08:12 -0500

    Python2 excercise 11

---
+++ Python-Training/Taller 1/ejercicio11-14.py
@@ -0,0 +1,73 @@
+
+""" 1. Campus requiere administrar algunos datos de sus Campers
+como por ejemplo, la creación, eliminación o búsqueda de los
+developers, entre otros, por tal razón, ha solicitado el diseño de
+un programa que cuente con el siguiente menú como panel de
+control: """
+
+""" 1.CREAR GRUPO ARTEMIS:
+    1.1 LISTAR CAMPERS DE ARTEMIS
+    1.2 AGREGAR CAMPERS A ARTEMIS 
+    1.3 ELIMINAR CAMPERS DE ARTEMIS
+    1.4 ORDENAR ALFABETICAMENTE EN LISTA DE ARTEMIS
+    1.5 BUSCAR CAMPER EN LISTA DE ARTEMIS
+
+    2. CREAR GRUPO SPUTNIK:
+    2.1 LISTAR CAMPERS DE SPUTNIK
+    2.2 AGREGAR CAMPERS A SPUTNKIK
+    2.3 ELIMINAR CAMPERS DE SPUTNIK
+    2.4 ORDENAR ALFABETICAMENTE EN SPUTNIK
+    2.5 BUSCAR CAMPER EN LISTA DE SPUTNIK
+    3. SALIR
+    DIGITE OPCION: 
+ """
+nombre = ""
+sputnik=[]
+opc = 0
+while opc !=3:
+    print ('------------------------------\n')
+    print(f'1.CREAR GRUPO ARTEMIS:\n 1.1 LISTAR CAMPERS DE ARTEMIS\n 1.2 AGREGAR CAMPERS A ARTEMIS \n 1.3 ELIMINAR CAMPERS DE ARTEMIS \n 1.4 ORDENAR ALFABETICAMENTE EN LISTA DE ARTEMIS \n 1.5 BUSCAR CAMPER EN LISTA DE ARTEMIS')
+    print(f'\t2.CREAR GRUPO SPUTNIK:\n\t2.1 LISTAR CAMPERS DE SPUTNIK\n\t2.2 AGREGAR CAMPERS A SPUTNKIK\n\t2.4 ORDENAR ALFABETICAMENTE EN SPUTNIK\n\t2.5 BUSCAR CAMPER EN LISTA DE SPUTNIK\n\t3.SALIR \n\t\tDIGITE UNA OPCION:')
+ 
+    opc = float(input())
+    if opc == 1:
+        print ('------------------------------\n')
+        print(f'Sputnik: {sputnik}\n')
+    elif opc == 1.1:
+        print ('\n------------------------------\nLISTA DE ESTUDIANTES ARTEMIS:')
+        for i in sputnik: 
+            print(f'* {i}')
+    
+    elif opc == 1.2:
+        print ('------------------------------\n')
+        nombre = input('Ingrese nombre completo del camper:')
+        sputnik.insert(0,nombre)
+        print(f'{nombre}+')
+        
+    elif opc == 1.3:
+        print ('------------------------------\n')
+        nombre = input('Ingrese el nombre a eliminar:')
+        x= sputnik.index(nombre)    
+        del sputnik[x]
+        print(f'{nombre}-')
+
+    elif opc ==1.4:
+        print ('------------------------------\n')
+        sputnik.sort()
+        print ('\n------------------------------\nLISTA DE ESTUDIANTES ARTEMIS ORDENADOS:')
+        for i in sputnik: 
+            print(f'* {i}')
+        
+    elif opc == 1.5:
+        print ('------------------------------\n')
+        nombre = input('Ingrese el nombre del camper: ')
+        for i in sputnik:
+            if nombre == i:
+                print (f'El camper: {i} \n Se encuentra en la lista ')
+          
+
+    else:
+        print ('------------------------------\n Ingrese una opcion valida' )
+        
+
+
