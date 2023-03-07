04d9ce5addec65eac5d0faf9a08c2e9952b38a0c
ruedaKaf <jhanru@live.com>
2023-03-07 09:05:06 -0500

    Taller basico

---
+++ tallerBasic/app.js
@@ -0,0 +1,41 @@
+let table = [{
+    "Traditional ": "90-100",
+    "14-Point Range":"12-14",
+    "Letter ": "A",
+    "SBG_Rating": 4,
+    "Proficiency_Level_With_Standard": "Exceeds proficiency"
+
+},
+{
+    "Traditional ": "80-89",
+    "14-Point Range":"9-11",
+    "Letter" : "B",
+    "SBG_Rating": 3,
+    "Proficiency_Level_With_Standard": "Demonstrates proficiency"
+
+},
+{
+    Traditional : "70-79",
+    "14-Point Range":"6-8",
+    Letter : "C",
+    SBG_Rating: 2,
+    Proficiency_Level_With_Standard: "Approaches proficiency"
+
+},
+{
+    Traditional : "60-69",
+    "14-Point Range":"3-5",
+    Letter : "D",
+    SBG_Rating: 1,
+    Proficiency_Level_With_Standard: "Falls well below proficiency"
+
+},
+{
+    Traditional : "80-89",
+    "14-Point Range":"9-11",
+    Letter : "B",
+    SBG_Rating: 3,
+    Proficiency_Level_With_Standard: "Demonstrates proficiency"
+
+},
+]
