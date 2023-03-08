1bcfe1506c3d1fad8fcb14970dd86e43e877b092
ruedaKaf <jhanru@live.com>
2023-03-08 06:11:52 -0500

    Metodo fromCodePoint () retorna una secuencia especial de code points

--- documentacionString/app.js
+++ documentacionString/app.js
@@ -1,5 +1,5 @@
 /* Method charAt() */
 
 /* const str = 'Hola mundo'; */
-console.log(String.fromCharCode(189, 43, 190, 61));
-// Expected output: "½+¾="
+console.log(String.fromCodePoint(9731, 9733, 9842, 0x2F804));
+// Expected output: "☃★♲你"
