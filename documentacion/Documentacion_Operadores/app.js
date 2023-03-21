3f445b604d16c5f0c496ecf84f921215c1e7df3e
ruedaKaf <jhanru@live.com>
2023-03-20 21:17:37 -0500

    Documenacion operadores Aritmeticos

---
+++ documentacion/Documentacion_Operadores/app.js
@@ -0,0 +1,95 @@
+/* DOCUMENTACION ARITMETICOS  */
+/* El operador suma (+) se utiliza para sumar dos valores. Si ambos valores son números, la suma se realiza normalmente. Si uno de los valores es una cadena de texto, el operador "+" concatenará las dos cadenas de texto.
+Ejemplo:
+ */
+function suma() {
+    let num1 = 10;
+    let num2 = 5;
+    let resultado = num1 + num2;
+    console.log(resultado); // Output: 15
+
+    let cadena1 = "Hola ";
+    let cadena2 = "mundo!";
+    let saludo = cadena1 + cadena2;
+    console.log(saludo); // Output: "Hola mundo!"
+
+
+}
+
+
+/* El operador resta (-) se utiliza para restar un valor de otro valor.
+Ejemplo: */
+function resta() {
+    let num1 = 10;
+    let num2 = 5;
+    let resultado = num1 - num2;
+    console.log(resultado); // Output: 5
+
+
+}
+
+
+/* El operador multiplicación (*) se utiliza para multiplicar dos valores.
+Ejemplo: */
+
+function multiplicación() {
+    let num1 = 10;
+    let num2 = 5;
+    let resultado = num1 * num2;
+    console.log(resultado); // Output: 50
+}
+
+
+/* El operador división (/) se utiliza para dividir un valor por otro valor.
+Ejemplo: */
+
+
+function division() {
+    let num1 = 10;
+    let num2 = 5;
+    let resultado = num1 / num2;
+    console.log(resultado); // Output: 2
+}
+
+
+
+
+/* El operador módulo (%) se utiliza para obtener el resto de una división.
+Ejemplo: */
+
+function modulo() {
+    let num1 = 10;
+    let num2 = 5;
+    let resto = num1 % num2;
+    console.log(resto); // Output: 0
+
+    let num3 = 11;
+    let num4 = 5;
+    let resto2 = num3 % num4;
+    console.log(resto2); // Output: 1
+}
+
+
+/* El operador incremento (++) se utiliza para aumentar el valor de una variable en 1.
+Ejemplo: */
+
+function incremento() {
+    let num1 = 10;
+    num1++;
+    console.log(num1); // Output: 11    
+}
+
+
+/* 
+El operador decremento (--) se utiliza para disminuir el valor de una variable en 1.
+Ejemplo: */
+
+function decremento() {
+    let num1 = 10;
+    num1--;
+    console.log(num1); // Output: 9
+}
+
+
+
+
